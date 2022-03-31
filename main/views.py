b8847994869bf8341ffb627751c233e436e9c75a
holynekk <e238109@metu.edu.tr>
2022-03-31 13:48:00 +0300

    creating & visualizing posts

--- main/views.py
+++ main/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, PostForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
+from .models import Post
 
-# Create your views here.
 
+@login_required(login_url="/login")
 def home(request):
-    return render(request, 'main/home.html')
+    posts = Post.objects.all()
+
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        post = Post.objects.get(id=post_id)
+        
+        if post and post.author == request.user:
+            post.delete()
+
+    return render(request, 'main/home.html', {"posts": posts})
 
 def sign_up(request):
     if request.method == 'POST':
@@ -18,3 +29,17 @@ def sign_up(request):
         form = RegisterForm()
 
     return render(request, 'registration/sign_up.html', {"form": form})
+
+@login_required(login_url="/login")
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/home')
+    else:
+        form = PostForm()
+    
+    return render(request, 'main/create_post.html', {"form": form})
